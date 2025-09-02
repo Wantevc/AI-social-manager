897ca596da1a3d2582d21cd156e7906bd380c00c
Wante Van Craen <wantevc@gmail.com>
2025-09-02 13:17:14 +0200

    chore(api): chapter 1 skeleton + health/version + config

---
+++ api/app/core/config.py
@@ -0,0 +1,10 @@
+﻿from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ENV: str = "dev"
+    DEBUG: bool = True
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
